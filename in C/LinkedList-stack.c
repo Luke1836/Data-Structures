b6bb7e905da053d920c8872f1ec3e694f36c8211
Luke1836 <codingluke2023@gmail.com>
2023-11-13 06:58:47 +0530

    Added LinkdeList files

---
+++ in C/LinkedList-stack.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stacks{
+    int data;
+    struct Stack *next;
+};
+
+struct Stacks *insert(struct Stacks *head, int item)
+{
+    struct Stacks *curr = head, *newNode;
+    newNode = (struct Stakcs*)malloc(sizeof(struct Stacks*));
+    newNode->data = item;
+    newNode->next = NULL;
+    if(head == NULL)
+    {
+        head = newNode;
+        head->next = NULL;
+    }
+    else{
+        newNode->next = head;
+        head = newNode;
+    }
+    return head;
+}
+
+struct Stacks *pop(struct Stacks *head)
+{
+    if(head == NULL)
+    {
+        printf("The stack is empty!\nError: Underflow\n");
+        return;
+    }
+    struct Stack *pred;
+    pred = (struct Stakcs*)malloc(sizeof(struct Stacks*));
+    pred = head;
+    head = head->next;    
+    free(pred);
+    return head;
+}
+
+void display(struct Stacks *head)
+{
+    struct Stacks *curr = head;
+    if(head == NULL)
+    {
+        printf("The stack is empty!\n");
+        return;
+    }
+    printf("The Stack elements:  ");
+    for(curr = head ; curr != NULL ; curr = curr->next)
+        printf("%d\t", curr->data);
+    printf("\n");
+}
+
+int main()
+{
+    struct Stacks *head = NULL;
+    int choice, element;
+    printf("Stack Operations\n1. Insert an element\n2. Delete an element\n3.Display\n4. Exit\nEnter your choice\n");
+    while(1)
+    {
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1: printf("Enter the element to be inserted:  ");
+                    scanf("%d", &element);
+                    head = insert(head, element);
+                    break;
+            case 2: head = pop(head);
+                    break;
+            case 3: display(head);
+                    break;
+            case 4: printf("The program execution has been terminated\n");
+                    exit(0);
+            default: printf("Invalid choice!\n");
+        }
+    }
+    return 1;
+}
--- in C/file.c
+++ in C/file.c
@@ -1,60 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+void binarySearch(char nameList[][100], char str[], int count, int lb, int ub)
+{
+while(lb <= ub)
+{
+int compare;
+int mid = (lb+ub)/2;
+compare = strcmp(str, nameList[mid]);
+if(compare == 0)
+{
+printf("The name is found in the file\n%s has been found.\n", str);
 
-#define MAX_STRINGS 100
-#define MAX_STRING_LENGTH 100
-
-void bubbleSort(char strings[MAX_STRINGS][MAX_STRING_LENGTH], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (strcmp(strings[j], strings[j + 1]) > 0) {
-                // Swap strings
-                char temp[MAX_STRING_LENGTH];
-                strcpy(temp, strings[j]);
-                strcpy(strings[j], strings[j + 1]);
-                strcpy(strings[j + 1], temp);
-            }
-        }
-    }
+exit(0);
 }
+else if(compare > 0)
+lb = mid+1;
+else
+ub = mid-1;
 
-int binarySearch(char strings[MAX_STRINGS][MAX_STRING_LENGTH], int left, int right, char target[MAX_STRING_LENGTH]) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        int compare = strcmp(strings[mid], target);
-        if (compare == 0) {
-            return mid; // String found at index mid
-        }
-        if (compare < 0) {
-            left = mid + 1; // String is in the right half
-        } else {
-            right = mid - 1; // String is in the left half
-        }
-    }
-    return -1; // String not found
 }
+printf("The name, %s is not present in the file\n", str);
+}
+void sort(char nameList[][100], int count)
+{
+int compare;
+char temp[100];
+//Sorting
+for(int i = 0 ; i < count-1 ; i++)
+{ for(int j = 0 ; j < count - 1 - i ; j++)
+{
+compare = strcmp(nameList[j], nameList[j+1]);
+if(compare > 0)
+{
+strcpy(temp, nameList[j]);
+strcpy(nameList[j], nameList[j+1]);
+strcpy(nameList[j+1], temp);
+}
+}
+}
+//Dipslaying the names in the list
+printf("The name list after sorting (you can check the original list by visiting the file): \n");
+for(int i = 0 ; i < count ; i++)
+{
+printf("%s\n", nameList[i]);
+}
+}
+int main()
+{
+char nameList[100][100];
+int count = 0; //Kepps count of the number of students in the list
+char str[100]; //Storing the name of the student to checked
+char filename[100];
+printf("Enter the file name\n");
+scanf("%s", filename);
+FILE *fptr = fopen(filename, "r");
+if(fptr == NULL)
+{
+printf("Error: The file doesn't exist!!!\n");
+return 1;
+}
+char line[100];
+while((fgets(line, sizeof(line), fptr)) != NULL){
+line[strcspn(line, "\n")] = '\0';
+strcpy(nameList[count], line);
+count ++;
+}
+fclose(fptr);
 
-int main() {
-    char strings[MAX_STRINGS][MAX_STRING_LENGTH];
-    int n = 0; // Number of strings
-
-    FILE *file = fopen("strings.txt", "r");
-    if (file == NULL) {
-        printf("Error opening file.\n");
-        return 1;
-    }
-
-    // Read strings from the file using fgets to read entire lines
-    char line[MAX_STRING_LENGTH];
-    while (fgets(line, sizeof(line), file) != NULL && n < MAX_STRINGS) {
-        line[strcspn(line, "\n")] = '\0';  // Remove the newline character if present
-        strcpy(strings[n], line);
-        n++;
-    }
-    fclose(file);
-
-    // Sort strings and search code (same as before)
-
-    return 0;
+//Sorting the names we read out from the file
+sort(nameList, count);
+printf("Enter the name to be searched\n");
+scanf("\n");
+gets(str);
+binarySearch(nameList, str, count, 0, (count-1));
+return 0;
 }
