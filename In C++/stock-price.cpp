33d4c4f49b7e08d5a0e2edbfc6b03702a3e953c4
Luke1836 <codingluke2023@gmail.com>
2023-11-23 08:09:18 +0530

    In C++ updated

--- In C++/stock-price.cpp
+++ In C++/stock-price.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 int main()
 {
-    
+    int min = INT32_MAX;
+    int max_profit = 0;
+    int arr[6];
+    cout << "Enter the prices of the stock in the 6 days" << endl;
+    for(int i = 0 ; i < 6 ; i++)
+        cin >> arr[i];
+    for(int i = 0 ; i < 6 ; i++)
+    {
+        if(arr[i] < min)
+           min = arr[i];
+        max_profit = max(max_profit, (arr[i] - min));
+    }
+    if(max_profit > 0)
+        cout << "Maximum Profit " << max_profit << endl;
+    else
+        cout << 0 << endl;
+
+    return 1;    
 }
---
+++ In C++/subarray_sum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, sum;
+    int count = 0;
+    cout << "Enter the size of the array" << endl;
+    cin >> n;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++)
+        cin >> arr[i];
+    cout << "Enter the sum to be found" << endl;
+    cin >> sum;
+    unordered_map< int , int > mp;
+    int curr_sum;
+    int ex_sum;
+    for(int i = 0 ; i < n ; i++)
+    {
+        curr_sum += arr[i];
+        if(curr_sum == sum);
+            count ++;
+    
+        ex_sum = curr_sum - sum;
+        if(mp.find(ex_sum) != mp.end())
+            count += mp[ex_sum];
+        mp[curr_sum]++;
+    }
+    cout << "Number of subarrays: " << count << endl;
+    return 1; 
+}
