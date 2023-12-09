34f67b6b5b8b6e48bcfafde9e9e7d85ea33e8293
Luke1836 <codingluke2023@gmail.com>
2023-12-09 16:10:46 +0530

    New files - Linked List added

--- in C/polynomial-List.c
+++ in C/polynomial-List.c
@@ -9,31 +9,31 @@ struct Node *next;
 
 // Function to insert a term into a polynomial
 struct Node *insertTerm(struct Node* head, int coefficient, int exponent) {
-struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
-newNode->coefficient = coefficient;
-newNode->exponent = exponent;
-newNode->next = NULL;
-if (head == NULL) {
-return newNode;
-}
-struct Node *curr = head;
-while (curr->next != NULL) {
-curr = curr->next;
-}
-curr->next = newNode;
-return head;
-}
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->coefficient = coefficient;
+    newNode->exponent = exponent;
+    newNode->next = NULL;
+    if (head == NULL) {
+        return newNode;
+    }
+    struct Node *curr = head;
+    while (curr->next != NULL) {
+        curr = curr->next;
+    }
+    curr->next = newNode;
+    return head;
+    }
 // Function to display a polynomial
 void displayPolynomial(struct Node *head) {
-struct Node *curr = head;
-while (curr != NULL) {
-printf("%dx^%d ", curr->coefficient, curr->exponent);
-if (curr->next != NULL) {
-printf("+ ");
-}
-curr = curr->next;
-}
-printf("\n");
+    struct Node *curr = head;
+    while (curr != NULL) {
+        printf("%dx^%d ", curr->coefficient, curr->exponent);
+        if (curr->next != NULL) {
+        printf("+ ");
+        }
+        curr = curr->next;
+    }
+    printf("\n");
 }
 // Function to add two polynomials
 struct Node *addPolynomials(struct Node *poly1, struct Node *poly2) {
@@ -44,44 +44,43 @@ int n1,n2, max;
 n1 = n2 = 0;
 while(curr1 != NULL)
 {
-n1++;
-curr1 = curr1->next;
+    n1++;
+    curr1 = curr1->next;
 }
 while(curr2 != NULL)
 {
-n2++;
-curr2 = curr2->next;
+    n2++;
+    curr2 = curr2->next;
 }
 if(n1 > n2)
-{ curr_max = poly1;
-curr_min = poly2;
+{   curr_max = poly1;
+    curr_min = poly2;
 }
 else {
-
-curr_max = poly2;
-curr_min = poly1;
+    curr_max = poly2;
+    curr_min = poly1;
 }
 while (curr_max != NULL) {
-int sumCoeff = 0;
-int coeff1 = (curr_max != NULL) ? curr_max->coefficient : 0;
-int exp1 = (curr_max != NULL) ? curr_max->exponent : 0;
-int coeff2 = (curr_min != NULL) ? curr_min->coefficient : 0;
-int exp2 = (curr_min != NULL) ? curr_min->exponent : 0;
-if(exp1>exp2)
-{
-result = insertTerm(result, coeff1, exp1);
-curr_max = curr_max->next;
-}
-else if(exp1<exp2)
-{
-result = insertTerm(result, coeff2, exp2);
-curr_min = curr_min->next;
-}
+    int sumCoeff = 0;
+    int coeff1 = (curr_max != NULL) ? curr_max->coefficient : 0;
+    int exp1 = (curr_max != NULL) ? curr_max->exponent : 0;
+    int coeff2 = (curr_min != NULL) ? curr_min->coefficient : 0;
+    int exp2 = (curr_min != NULL) ? curr_min->exponent : 0;
+    if(exp1>exp2)
+    {
+        result = insertTerm(result, coeff1, exp1);
+        curr_max = curr_max->next;
+    }
+    else if(exp1<exp2)
+    {
+        result = insertTerm(result, coeff2, exp2);
+        curr_min = curr_min->next;
+    }
 else {
-sumCoeff = coeff1 + coeff2;
-result = insertTerm(result, sumCoeff, exp1);
-curr_max = curr_max->next;
-curr_min = curr_min->next;
+    sumCoeff = coeff1 + coeff2;
+    result = insertTerm(result, sumCoeff, exp1);
+    curr_max = curr_max->next;
+    curr_min = curr_min->next;
 }
 }
 printf("%d %d\n", result->coefficient, result->exponent);
@@ -92,25 +91,24 @@ struct Node *multiplyPolynomials(struct Node *poly1, struct Node *poly2) {
 struct Node *result = NULL;
 struct Node *curr1 = poly1;
 while (curr1 != NULL) {
-struct Node* curr2 = poly2;
-while (curr2 != NULL) {
-int productCoeff = curr1->coefficient * curr2->coefficient;
-int sumExp = curr1->exponent + curr2->exponent;
-result = insertTerm(result, productCoeff, sumExp);
-curr2 = curr2->next;
-}
-curr1 = curr1->next;
-}
-return result;
-}
+    struct Node* curr2 = poly2;
+    while (curr2 != NULL) {
+    int productCoeff = curr1->coefficient * curr2->coefficient;
+    int sumExp = curr1->exponent + curr2->exponent;
+    result = insertTerm(result, productCoeff, sumExp);
+    curr2 = curr2->next;
+    }
+    curr1 = curr1->next;
+    }
+    return result;
+    }
 // Function to free the memory allocated for a polynomial
 void freePolynomial(struct Node *head) {
 struct Node *curr = head;
 while (curr != NULL) {
-struct Node *temp = curr;
-
-curr = curr->next;
-free(temp);
+    struct Node *temp = curr;
+    curr = curr->next;
+    free(temp);
 }
 }
 int main() {
@@ -120,22 +118,22 @@ struct Node *poly2 = NULL;
 int coeff, exp;
 printf("Enter the first polynomial (enter coefficient-0 and exponent-0 at the end):\n");
 while (1) {
-printf("Enter coefficient and exponent: ");
-scanf("%d %d", &coeff, &exp);
-if (coeff == 0 && exp == 0) {
-break;
-}
-poly1 = insertTerm(poly1, coeff, exp);
+    printf("Enter coefficient and exponent: ");
+    scanf("%d %d", &coeff, &exp);
+    if (coeff == 0 && exp == 0) {
+        break;
+    }
+    poly1 = insertTerm(poly1, coeff, exp);
 }
 // Input second polynomial
 printf("\nEnter the second polynomial (enter coefficient-0 and exponent-0 at the end):\n");
 while (1) {
-printf("Enter coefficient and exponent: ");
-scanf("%d %d", &coeff, &exp);
-if (coeff == 0 && exp == 0) {
-break;
-}
-poly2 = insertTerm(poly2, coeff, exp);
+    printf("Enter coefficient and exponent: ");
+    scanf("%d %d", &coeff, &exp);
+    if (coeff == 0 && exp == 0) {
+    break;
+    }
+    poly2 = insertTerm(poly2, coeff, exp);
 }
 // Display the input polynomials
 printf("\nFirst Polynomial: ");
