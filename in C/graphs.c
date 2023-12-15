12bb5e96d2e9d9ec3efc741258749fd5baeecddc
Luke1836 <codingluke2023@gmail.com>
2023-12-15 12:49:22 +0530

    Sortings added

---
+++ in C/graphs.c
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VERTICES 100
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Graph {
+    int numVertices;
+    struct Node* adjacencyList[MAX_VERTICES];
+};
+
+struct Queue {
+    int front, rear;
+    int capacity;
+    int* array;
+};
+
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+
+    for (int i = 0; i < vertices; ++i)
+        graph->adjacencyList[i] = NULL;
+
+    return graph;
+}
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addEdge(struct Graph* graph, int src, int dest) {
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjacencyList[src];
+    graph->adjacencyList[src] = newNode;
+
+    // For undirected graph, add an edge from dest to src as well
+    newNode = createNode(src);
+    newNode->next = graph->adjacencyList[dest];
+    graph->adjacencyList[dest] = newNode;
+}
+
+void displayAdjacencyList(struct Graph* graph) {
+    for (int i = 0; i < graph->numVertices; ++i) {
+        printf("Adjacency List of vertex %d: ", i);
+        struct Node* temp = graph->adjacencyList[i];
+        while (temp != NULL) {
+            printf("%d -> ", temp->data);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+void displayAdjacencyMatrix(struct Graph* graph) {
+    int adjacencyMatrix[MAX_VERTICES][MAX_VERTICES] = {0};
+
+    for (int i = 0; i < graph->numVertices; ++i) {
+        struct Node* temp = graph->adjacencyList[i];
+        while (temp != NULL) {
+            adjacencyMatrix[i][temp->data] = 1;
+            temp = temp->next;
+        }
+    }
+
+    printf("Adjacency Matrix:\n");
+    for (int i = 0; i < graph->numVertices; ++i) {
+        for (int j = 0; j < graph->numVertices; ++j) {
+            printf("%d ", adjacencyMatrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void DFSUtil(struct Graph* graph, int vertex, int visited[]) {
+    visited[vertex] = 1;
+    printf("%d ", vertex);
+
+    struct Node* temp = graph->adjacencyList[vertex];
+    while (temp != NULL) {
+        if (!visited[temp->data])
+            DFSUtil(graph, temp->data, visited);
+        temp = temp->next;
+    }
+}
+
+void DFS(struct Graph* graph, int startVertex) {
+    int visited[MAX_VERTICES] = {0};
+    printf("Depth First Search starting from vertex %d: ", startVertex);
+    DFSUtil(graph, startVertex, visited);
+    printf("\n");
+}
+
+struct Queue* createQueue(int capacity) {
+    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
+    queue->capacity = capacity;
+    queue->front = queue->rear = -1;
+    queue->array = (int*)malloc(capacity * sizeof(int));
+    return queue;
+}
+
+int isQueueEmpty(struct Queue* queue) {
+    return (queue->front == -1);
+}
+
+void enqueue(struct Queue* queue, int item) {
+    queue->array[++queue->rear] = item;
+    if (queue->front == -1)
+        ++queue->front;
+}
+
+int dequeue(struct Queue* queue) {
+    int item = queue->array[queue->front];
+    if (queue->front == queue->rear)
+        queue->front = queue->rear = -1;
+    else
+        ++queue->front;
+    return item;
+}
+
+void BFS(struct Graph* graph, int startVertex) {
+    int visited[MAX_VERTICES] = {0};
+    struct Queue* queue = createQueue(MAX_VERTICES);
+
+    printf("Breadth First Search starting from vertex %d: ", startVertex);
+    visited[startVertex] = 1;
+    enqueue(queue, startVertex);
+
+    while (!isQueueEmpty(queue)) {
+        int currentVertex = dequeue(queue);
+        printf("%d ", currentVertex);
+
+        struct Node* temp = graph->adjacencyList[currentVertex];
+        while (temp != NULL) {
+            int adjacentVertex = temp->data;
+            if (!visited[adjacentVertex]) {
+                visited[adjacentVertex] = 1;
+                enqueue(queue, adjacentVertex);
+            }
+            temp = temp->next;
+        }
+    }
+
+    printf("\n");
+}
+
+int main() {
+    int vertices, edges;
+    printf("Enter the number of vertices in the graph: ");
+    scanf("%d", &vertices);
+
+    struct Graph* graph = createGraph(vertices);
+
+    printf("Enter the number of edges in the graph: ");
+    scanf("%d", &edges);
+
+    printf("Enter the edges (source destination):\n");
+    for (int i = 0; i < edges; ++i) {
+        int src, dest;
+        scanf("%d %d", &src, &dest);
+        addEdge(graph, src, dest);
+    }
+
+    printf("\nGraph Operations:\n");
+    while (1) {
+        printf("1. Compute adjacency list and adjacency matrix\n");
+        printf("2. Perform a depth-first search\n");
+        printf("3. Perform a breadth-first search\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+
+        int choice;
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                displayAdjacencyList(graph);
+                displayAdjacencyMatrix(graph);
+                break;
+            case 2:
+                printf("Enter the starting vertex for depth-first search: ");
+                int startDFS;
+                scanf("%d", &startDFS);
+                DFS(graph, startDFS);
+                break;
+            case 3:
+                printf("Enter the starting vertex for breadth-first search: ");
+                int startBFS;
+                scanf("%d", &startBFS);
+                BFS(graph, startBFS);
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("Invalid choice. Please enter a valid option.\n");
+        }
+    }
+
+    return 0;
+}
---
+++ in C/sort.c
@@ -0,0 +1,212 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function prototypes
+void insertionSort(int arr[], int n);
+void selectionSort(int arr[], int n);
+void heapSort(int arr[], int n);
+void mergeSort(int arr[], int low, int high);
+void merge(int arr[], int low, int mid, int high);
+void quickSort(int arr[], int low, int high);
+int partition(int arr[], int low, int high);
+
+// ... (Other functions and declarations)
+
+int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    while (1) {
+        printf("\nSorting Algorithms:\n");
+        printf("1. Insertion Sort\n");
+        printf("2. Selection Sort\n");
+        printf("3. Heap Sort\n");
+        printf("4. Merge Sort\n");
+        printf("5. Quick Sort\n");
+        printf("6. Exit\n");
+
+        printf("Enter your choice: ");
+        int choice;
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                insertionSort(arr, n);
+                break;
+            case 2:
+                selectionSort(arr, n);
+                break;
+            case 3:
+                heapSort(arr, n);
+                break;
+            case 4:
+                mergeSort(arr, 0, n - 1);
+                break;
+            case 5:
+                quickSort(arr, 0, n - 1);
+                break;
+            case 6:
+                exit(0);
+            default:
+                printf("Invalid choice. Please enter a valid option.\n");
+        }
+
+        printf("Sorted array:\n");
+        for (int i = 0; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex, temp;
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    // Check if left child is larger than the root
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    // Check if right child is larger than the largest so far
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    // If the largest is not the root, swap them and continue to heapify
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
+    }
+}
+void merge(int arr[], int low, int mid, int high) {
+    int i, j, k;
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++) {
+        L[i] = arr[low + i];
+    }
+    for (j = 0; j < n2; j++) {
+        R[j] = arr[mid + 1 + j];
+    }
+
+    i = 0; 
+    j = 0; 
+    k = low; 
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int low, int high) {
+    if (low < high) {
+        int mid = low + (high - low) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1; 
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1; 
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
+
