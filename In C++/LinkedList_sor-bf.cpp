718199be882c4136ef3f1b8bad7de701267f1170
Luke1836 <codingluke2023@gmail.com>
2023-12-25 22:26:49 +0530

    Sorting programs added

--- In C++/LinkedList_sor-bf.cpp
+++ In C++/LinkedList_sor-bf.cpp
@@ -9,12 +9,13 @@ class Node {
 };
 
 
-void push(Node *head, int data)
+Node *push(Node *head, int data)
 {
     Node *newNode = new Node();
     newNode->data = data;
     newNode->next = head;
     head = newNode;
+    return head;
 }
 
 void display(Node *head)
@@ -33,6 +34,62 @@ void display(Node *head)
     }
 }
 
+Node *mergeList(Node *p1, Node *p2)
+{
+    Node *result = NULL;
+    if(p1 == NULL)
+        return p2;
+    else if(p2 == NULL)
+        return p1;
+    
+    if(p1->data <= p2->data)
+    {
+        result = p1;
+        result->next = mergeList(p1->next, p2);
+    }
+    else {
+        result = p2;
+        result->next = mergeList(p1, p2->next);
+    }
+    return result;
+}
+
+void divideList(Node *head, Node *&start, Node *&end)
+{
+    Node *fast, *slow;
+    slow = head;
+    fast = head->next;
+
+    while (fast != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+
+    start = head;
+    end = slow->next;
+    slow->next = NULL;
+}
+
+
+void mergeSort(Node *head)
+{
+    Node *tempHead = head, *p1, *p2;
+
+    if((tempHead == NULL) || (tempHead->next == NULL))
+        return;
+    
+    divideList(tempHead, p1, p2);
+    mergeSort(p1);
+    mergeSort(p2);
+
+    tempHead = mergeList(p1, p2);
+}
+
 int main(void)
 {
     Node *head = NULL;
@@ -40,14 +97,18 @@ int main(void)
     cout << "Enter the size of the list " << endl;
     cin >> n;
 
+    cout << "Enter the element" << endl;
     for(int i = 0 ; i < n ; i++)
     {
         int data;
-        cout << "Enter the element" << endl;
         cin >> data;
-        push(head, data);
+        head = push(head, data);
     }
     cout << "Original List: " << endl;
     display(head);
 
+    mergeSort(head);
+    cout << "Sorted array: " << endl;
+    display(head);
+    return 1;
 }
