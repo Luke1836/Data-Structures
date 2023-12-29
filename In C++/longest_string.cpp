5bb0483ca00a1dfa441d926cfbe13a01d1baaa7e
Luke1836 <codingluke2023@gmail.com>
2023-12-29 23:38:34 +0530

    CP programs added

--- In C++/longest_string.cpp
+++ In C++/longest_string.cpp
@@ -3,9 +3,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
 int main(void)
 {
-    
+    string str;
+    cout << "Enter a string:" << endl;
+    cin >> str;
+    int len = str.length();
+    int maxLen = 0;
+    vector<int> mapp(256,-1); //Stores the last index of the characters
+    int l = 0;
+
+    for(int r= 0 ; r < len ; r++)
+    {
+        l = max(l, mapp[str[r]] + 1);
+        maxLen = max(maxLen, r-l-1);
+        mapp[str[r]] = r;
+    }
+
+    cout << "Maximum length: " << maxLen << endl;
+    return 1;
+
 }
