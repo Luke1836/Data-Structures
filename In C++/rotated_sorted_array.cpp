8771fe4aaf2f58e01edab279abd20a7aee079623
Luke1836 <codingluke2023@gmail.com>
2023-12-31 13:35:29 +0530

    Rotated sorted array qn added

--- In C++/rotated_sorted_array.cpp
+++ In C++/rotated_sorted_array.cpp
@@ -6,5 +6,14 @@ using namespace std;
 
 int main(void)
 {
-    
+    int n;
+    cout << "Enter the size of the array" << endl;
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter the elements" << endl;
+    for(int i = 0 ; i < n ; i++)
+        cin >> arr[i];
+
+    //Checking whether it is a rotated sorted array
 }
