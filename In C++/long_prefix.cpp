fd465d87719a2a1f315d5b08cb51a9c42661d4ae
Luke1836 <codingluke2023@gmail.com>
2024-01-04 20:21:47 +0530

    longest-prefix added

---
+++ In C++/long_prefix.cpp
@@ -0,0 +1,17 @@
+//Program to find the longest common prefix of a string array
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cout << "Enter the number of elements in tha array" << endl;
+    cin >> n;
+    string name[n];
+
+    for(int i = 0 ; i < n ; i++)
+        cin >> name[i];
+    
+    
+}
