87bffbe6356adc7ce036b2a8bc309d28f0e81a01
Luke1836 <codingluke2023@gmail.com>
2024-01-04 20:14:04 +0530

    Coded added

--- In C++/max_avg.cpp
+++ In C++/max_avg.cpp
@@ -17,10 +17,26 @@ int main(void)
     int n1; //Store the size of the subarray.
     cout << "Enter the size of the subarray:" << endl;
     cin >> n1;
+    double sum = 0.0;
+    double max_sum = 0.0;
+    int c = 0;
 
     for(int i = 0 ; i < n ; i++)
     {
-        
+        if(i < n1)
+        {
+            sum += arr[i];
+            if(max_sum < sum)
+                max_sum = sum;
+        }    
+        else
+        {   sum = sum - arr[c] + arr[i];
+            c++;
+            if(max_sum < sum)
+                max_sum = sum;
+        }
     }
-
+    double avg = max_sum / n1;
+    cout << "Maximum Average = " << avg << endl;
+    return 0;
 }
