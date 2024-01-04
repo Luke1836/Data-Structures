f488173abc9036a1ab8119516e7ebc17359bb891
Luke1836 <codingluke2023@gmail.com>
2024-01-04 17:34:17 +0530

    Maximum subarray

---
+++ In C++/max_avg.cpp
@@ -0,0 +1,19 @@
+//Program to find the subarray with the maximum average value
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cout << "Enter the size of the array" << endl;
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter the elements into tbe array" << endl;
+    for(int i = 0 ; i < n ; i++)
+        cin >> arr[i];
+
+    int n1; //Size of the subarray.
+
+}
