99c4c840b40950bed23b06409185e6c331342a39
Luke1836 <codingluke2023@gmail.com>
2024-01-04 17:16:27 +0530

    Updated

--- In C++/rotated_sorted_array.cpp
+++ In C++/rotated_sorted_array.cpp
@@ -10,10 +10,40 @@ int main(void)
     cout << "Enter the size of the array" << endl;
     cin >> n;
     int arr[n];
+    int element;
 
     cout << "Enter the elements" << endl;
     for(int i = 0 ; i < n ; i++)
         cin >> arr[i];
 
+    cout << "Enter the element to be searched" << endl;
+    cin>>element;
+
     //Checking whether it is a rotated sorted array
+    int lb = 0 ;
+    int ub = n-1;
+    int mid;
+
+    while(lb <= ub)
+    {
+        mid = (lb+ub)/2;
+        if(arr[mid] == element)
+        {
+            cout << (mid+1) << endl;
+            exit(1);
+        }
+        else if(arr[lb] > arr[mid])
+        {
+            if(element > arr[mid] && arr[lb] < element)
+                ub = mid - 1;
+            else
+                lb = mid + 1;
+
+        }
+        else 
+            lb = mid + 1;
+    }
+
+    cout << "-1" << endl;
+    return 1;
 }
