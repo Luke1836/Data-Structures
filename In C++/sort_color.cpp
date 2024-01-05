7010aa22f65d6680bb74d290108d5e4d9d585f8f
Luke1836 <codingluke2023@gmail.com>
2024-01-06 00:24:02 +0530

    color sort code added

--- In C++/sort_color.cpp
+++ In C++/sort_color.cpp
@@ -1,5 +1,6 @@
 /*
     Program to sort the colors such that the same colors are adjacent to each other.
+    Using the Dutch National Flag Algorithm
     Here we consider the Dutch National Flag. Color Scheme: Red White Blue.
     0 - Red, 1- White, 2 - Blue
 */
@@ -7,6 +8,13 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+
+/* void swapNum(int *p1, int *p2)
+{
+    int *temp = p1;
+    p1 = p2;
+    p2 = temp;
+} */
 
 int main(void)
 {
@@ -17,5 +25,34 @@ int main(void)
     cout << "Enter the colors: " << endl;
     for(int i = 0 ; i < n ; i++)
         cin >> arr[i];
-    
+    int low = 0, mid = 0, high = n - 1, temp;
+
+    while(mid <= high)
+    {
+        if(arr[mid] == 0)
+        {
+            temp = arr[low];
+            arr[low] = arr[mid];
+            arr[mid] = temp;
+            low++;
+            mid++;
+        }
+        else if(arr[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            temp = arr[high];
+            arr[high] = arr[mid];
+            arr[mid] = temp;
+            high--;
+        }
+    }
+
+    cout << "Color Scheme: ";
+    for(int i = 0 ; i < n ; i++)
+        cout << arr[i] << "  ";
+    cout << endl;
+    return 0;    
 }
