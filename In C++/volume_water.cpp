26b3d3f13c5acb558711f1c5ac17e7642401888e
Luke1836 <codingluke2023@gmail.com>
2024-01-05 19:09:17 +0530

    Maximum volume question added

---
+++ In C++/volume_water.cpp
@@ -0,0 +1,20 @@
+//Program to find the container which can hold the largest volume of water
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cout << "Enter the count of the heights for the container" << endl;
+    int height[n];
+
+    cout << "Enter the heights" << endl;
+    for(int i = 0 ; i < n ; i++)
+        cin >> height[i];
+    int curr_vol = 0;
+    int max_vol = INT32_MIN;
+
+    
+
+}
