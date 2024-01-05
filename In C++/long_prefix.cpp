12b40eae5e083eb8726ca513060ee57940c97ba6
Luke1836 <codingluke2023@gmail.com>
2024-01-05 18:45:45 +0530

    Longest substring code added

--- In C++/long_prefix.cpp
+++ In C++/long_prefix.cpp
@@ -13,5 +13,23 @@ int main(void)
     for(int i = 0 ; i < n ; i++)
         cin >> name[i];
     
-    
+    //We sort the array
+    sort(name, name + n);
+    string s1 = name[0], s2 = name[n-1];
+    string result = "";
+
+    if(s1.length() == 0)
+    {
+        cout << "" << endl;
+        exit(0);
+    }
+
+    for(int i = 0 ; i < s1.length() ; i++)
+    {
+        if(s1[i] == s2[i])
+            result += s1[i];
+    }
+
+    cout << result << endl;
+    return 0;
 }
