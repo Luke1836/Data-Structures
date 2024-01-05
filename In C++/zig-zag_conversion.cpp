b3a2a40aeaae70d5f2f3b92a5f3a3055aafdc91e
Luke1836 <codingluke2023@gmail.com>
2024-01-06 00:57:12 +0530

    Zig-zag string code added

--- In C++/zig-zag_conversion.cpp
+++ In C++/zig-zag_conversion.cpp
@@ -1,17 +1,45 @@
-//Program to print a string in a  zig-zag format, similiar to a wave array
+//Program to print a string in a  zig-zag format depending upn the number of rows the user input.
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(void)
 {
-    string str, newStr;
+    string str;
     cout << "Enter a string" << endl;
     cin >> str;
     int len = str.length();
 
+    int n, c = 0;
+    cout << "Enter the number of rows: " << endl;
+    cin >> n;
+    string newStr[n];
+    bool down;      // Keeps check whether the next movement should be up or down
+
+    if(len == 1)
+    {
+        cout << str << endl;
+        exit(0);
+    }
+
     for(int i = 0 ; i < len ; i++)
     {
-        
+        newStr[c] += str[i];
+        if(c == n-1)
+            down = false;
+        else if(c == 0)
+            down = true;
+        if(down)
+            c++;
+        else
+            c--;        
     }
+
+    cout << "The new String: ";
+    for(int i = 0 ; i < n ; i++)
+    {
+        cout << newStr[i];
+    }
+    cout << endl;
+    return 0;
 }
