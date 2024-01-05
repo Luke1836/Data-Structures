c97fc23b000c9facd190f1e017a193538b619987
Luke1836 <codingluke2023@gmail.com>
2024-01-06 00:29:20 +0530

    Zig-zag string question added

---
+++ In C++/zig-zag_conversion.cpp
@@ -0,0 +1,17 @@
+//Program to print a string in a  zig-zag format, similiar to a wave array
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    string str, newStr;
+    cout << "Enter a string" << endl;
+    cin >> str;
+    int len = str.length();
+
+    for(int i = 0 ; i < len ; i++)
+    {
+        
+    }
+}
