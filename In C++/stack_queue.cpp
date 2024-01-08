401baae213071ca8e375d2ecf44c8b68d363055b
Luke1836 <codingluke2023@gmail.com>
2024-01-08 14:23:38 +0530

    Push operation added

---
+++ In C++/stack_queue.cpp
@@ -0,0 +1,38 @@
+//Program to implement a stack from queues
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack
+{
+    queue<int> q1, q2;  //In-built queues
+    int curr_size;
+    public:
+        Stack()
+        {
+            curr_size = 0;
+        }
+
+        void push(int data)
+        {
+            curr_size ++;
+            q2.push(data);
+
+            while(!q1.empty())
+            {
+                q2.push(q1.front());
+                q1.pop();
+            }
+            queue<int> temp;
+            temp = q1;
+            q1 = q2;
+            q2 = temp;
+        }
+        
+};
+
+
+int main(void)
+{
+
+}
