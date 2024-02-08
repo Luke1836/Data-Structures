6997e2b5cd1afc9a7803f61f5c9a3991ec6e6cb1
Luke1836 <codingluke2023@gmail.com>
2024-02-08 17:30:05 +0530

    Graphs - changes made

---
+++ in C/graphs_revised.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Graph {
+    int v;
+    int e;
+    int **adjMatrix;
+};
+
+struct edge {
+    int source;
+    int destination;
+};
+
+int main(void)
+{
+    
+    return 0;
+}
